70d3798ea1b5c04970c9026e75d4f8f0830b65ec
Emma Tomlinson <t97emma@gmail.com>
2026-01-12 20:33:32 +0000

    Added the rotation logic, not I just need to implement the counters

--- 2025/src/day1.ml
+++ 2025/src/day1.ml
@@ -1,31 +1,106 @@
-open! Core
-open! Hardcaml
-open! Hardcaml.Signal
+open Hardcaml
+open Hardcaml.Signal
 
 module I = struct
     type 'a t = {
         clock : 'a;
         clear : 'a;
-        ready : 'a;
-        data  : 'a[@bits 8];
+        (* indicates there is a byte ready to receive *)
+        ready_rx : 'a;
+        (* indicates that a byte can be sent *)
+        ready_tx : 'a;
+        data : 'a[@bits 8];
     }
     [@@deriving hardcaml]
 end
 
 module O = struct
     type 'a t = {
-        result : 'a[@bits 32]
+        (* indicates the computation and result output is complete *)
+        done_ : 'a[@rtlname "done"];
+        (* indicates that a byte is being output for tx *)
+        tx : 'a;
+        (* this outputs the result byte by byte once computation is done! *)
+        data : 'a[@bits 8];
     }
     [@@deriving hardcaml]
 end
 
+module States = struct
+    type t =
+        | SignByte
+        | ReadNumber
+        | TxByte
+        | WaitForReady
+    [@@deriving sexp_of, compare, enumerate]
+end
+
+let apply_rotation n rot = 
+    let mod_100 n =
+        (* This code is translated from https://godbolt.org/z/c385vcYo4 for fast division *)
+        (* TODO: I'm unsure if hardcaml/later synthesis will optimise this kind
+           of usecase but I wanted to be sure I had something that did not actually
+           make a divisor*)
+        (* If not this could be done automatically for known values (i.e. something
+           like a function `%:.` similar to `+:.`) could generate a circuit like
+           this automatically for (width n) *)
+        (* NOTE: Because out range of values is so small after addition -999 to 1098
+           we could just make a look up system to see in which 100 it is and just
+           subtract by that *)
+        let mult = sresize (n *+ (of_int ~width:32 5243)) 32 in
+        let tmp_a = srl mult 31 in
+        let tmp_b  = sra mult 19 in
+        let div = tmp_a +: tmp_b in
+        (* once we have the division we can then multiply by 100 and see the difference is the remainder *)
+        let lower_bound = sresize (div *+ (of_int ~width:32 100)) 32 in
+        n -: lower_bound
+        in
+    let modulo_100 n =
+        let tmp = mod_100 n in
+        (* If it is negative (msb is set) then shift it back up by 100 to get the actual modulo (as if winding properly) *)
+        mux2 (msb tmp) (tmp +:. 100) tmp in
+    modulo_100 (n +: rot)
+
 let create (i : _ I.t) = 
-    let acc = reg_fb
-        (Reg_spec.create ~clock:i.clock ~clear:i.clear ())
-        ~enable:i.ready
-        ~width:32
-        ~f:(fun n -> n +: uresize i.data 32) in
-    {
-        O.result = acc
-    }
+    (* multiplying by 10 is the same multiply by 8 and then adding the it multiplied twice *)
+    let mul_10 (n : Reg_spec.signal) = (sll n 3) +: (sll n 1) in
+    (* The magic numbers are taken from godbolt to prevent me from having to figure it out myself *)
+    let r_sync = Reg_spec.create ~clock:i.clock ~clear:i.clear () in
+    let sign = Always.Variable.reg ~enable:vdd ~width:1 r_sync in
+    let num = Always.Variable.reg ~enable:vdd ~width:10 r_sync in
+    let acc = Always.Variable.reg ~enable:vdd ~width:32 r_sync in
+    let sm = Always.State_machine.create (module States) ~enable:vdd r_sync in
+    Always.(
+        compile [
+            sm.switch [
+                (
+                    States.SignByte,
+                    [
+                        if_ i.ready_rx [
+                            sign <-- (i.data ==: (of_char 'R'));
+                            num <-- zero 10;
+                            sm.set_next States.ReadNumber
+                        ]
+                        []
+                    ]
+                );
+                (
+                    States.ReadNumber,
+                    [
+                        if_ i.ready_rx [
+                            if_ (i.data ==: (of_char '\n')) [
+                                (* Do the computation! *)
+                                (* add (or subtract) the current value to to the current accumulator *)
+                                acc <-- apply_rotation num.value (mux2 sign.value num.value (negate num.value))
+                            ] [
+                                (* continue to gather the number! *)
+                                num <-- (mul_10 num.value) +: (i.data -: (of_char '0')); 
+                            ]
+                        ]
+                        []
+                    ]
+                )
+            ]
+        ]
+    );
 
--- 2025/test/day1_test.ml
+++ 2025/test/day1_test.ml
@@ -2,20 +2,43 @@ open! Hardcaml
 module Day1 = Fpga_2025_lib.Day1
 module Sim = Cyclesim.With_interface(Day1.I)(Day1.O)
 
-let%test "test" = 
-    let sim = Sim.create Day1.create in
-    let inputs : _ Day1.I.t = Cyclesim.inputs sim in
-    let outputs : _ Day1.O.t = Cyclesim.outputs sim in
-    let step (n : int)=
-        inputs.clear := Bits.gnd;
-        inputs.ready := Bits.gnd;
-        inputs.clock := Bits.gnd;
-        inputs.data  := Bits.of_int ~width:8 n;
-        Cyclesim.cycle sim;
-        inputs.ready := Bits.vdd;
-        inputs.clock := Bits.vdd;
-        Cyclesim.cycle sim
-    in
-    (step 5);
-    (step 100);
-    Bits.equal outputs.result.contents (Bits.of_int ~width:32 105)
+let modulo a b = let tmp = a mod b in
+    if tmp < 0 then tmp + b
+    else tmp
+
+let rotation_test start rot =
+    let expected = modulo (start + rot) 100 in
+    let result = Day1.apply_rotation (Signal.of_int ~width:32 start) (Signal.of_int ~width:32 rot) in
+    print_string "Expected value: ";
+    print_int expected;
+    print_endline "";
+    print_string "Actual value: ";
+    print_int (Signal.to_sint result);
+    print_endline "";
+    Int.equal (Signal.to_sint result) expected
+
+let%test "Apply positive rotation test" = rotation_test 50 99
+
+let%test "Apply negative rotation test" = rotation_test 10 (-40)
+
+let %test "Apply large positive rotation test" = rotation_test 0 220
+
+let %test "Apply large negative rotation test" = rotation_test 10 (-940)
+
+(* let%test "test" =  *)
+(*     let sim = Sim.create Day1.create in *)
+(*     let inputs : _ Day1.I.t = Cyclesim.inputs sim in *)
+(*     let outputs : _ Day1.O.t = Cyclesim.outputs sim in *)
+(*     let step (n : int)= *)
+(*         inputs.clear := Bits.gnd; *)
+(*         inputs.ready := Bits.gnd; *)
+(*         inputs.clock := Bits.gnd; *)
+(*         inputs.data  := Bits.of_int ~width:8 n; *)
+(*         Cyclesim.cycle sim; *)
+(*         inputs.ready := Bits.vdd; *)
+(*         inputs.clock := Bits.vdd; *)
+(*         Cyclesim.cycle sim *)
+(*     in *)
+(*     (step 5); *)
+(*     (step 100); *)
+(*     Bits.equal outputs.result.contents (Bits.of_int ~width:32 105) *)
